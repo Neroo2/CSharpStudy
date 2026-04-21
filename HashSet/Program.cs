745c8569ff7e347cb15c289cbdbb1c48ba2d33d0
Junior <nelsonnnnn.junior@gmail.com>
2026-04-20 22:01:46 -0300

    HashSet
    Finished HashSet activity

---
+++ HashSet/Program.cs
@@ -0,0 +1,35 @@
+﻿System.Console.Write("How many students for course A? ");
+int courseA = int.Parse(Console.ReadLine());
+System.Console.Write("How many students for course B? ");
+int courseB = int.Parse(Console.ReadLine());
+System.Console.Write("How many students for course C? ");
+int courseC = int.Parse(Console.ReadLine());
+
+HashSet<int> alunos = new();
+
+
+for (int i = 1; i <= courseA; i++)
+{
+    Console.WriteLine("A: ");
+    int number = int.Parse(Console.ReadLine());
+    alunos.Add(number);
+
+}
+
+for (int i = 1; i <= courseB; i++)
+{
+    Console.WriteLine("B: ");
+    int number = int.Parse(Console.ReadLine());
+    alunos.Add(number);
+
+}
+
+for (int i = 1; i <= courseC; i++)
+{
+    Console.WriteLine("C: ");
+    int number = int.Parse(Console.ReadLine());
+    alunos.Add(number);
+
+}
+
+Console.WriteLine(alunos.Count);
